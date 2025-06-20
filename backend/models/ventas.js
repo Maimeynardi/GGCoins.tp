c6779c6e26afbb615f5f438f2d93375286f9dfc6
emmanuelalbe <emmanuelmalve@gmail.com>
2025-06-20 18:46:47 -0300

    Creacion Modelo Venta.js

---
+++ backend/models/ventas.js
@@ -0,0 +1,28 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../configuracionDataBase/BaseDeDatos.js');
+
+const VENTAS = sequelize.define('VENTAS',{
+    NOMBRE_CLIENTE:{
+        allowNull:false,
+        type: DataTypes.STRING(50)
+    },
+    ID_VENTAS:{
+        allowNull:false,
+        primaryKey:true,
+        type: DataTypes.INTEGER,
+        autoIncrement:true
+    },
+    DETALLE:{
+        allowNull:false,
+        type: DataTypes.INTEGER,
+        references:{
+            model:'DETALLES_VENTA',
+            key:'ID_DETALLE'
+        }
+    },
+
+},
+   {tableName:'VENTAS'}
+);
+
+module.exports = VENTAS;
