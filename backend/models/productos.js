f3b1caf884329f3655402482c83c79e160b5b0b4
Maimeynardi <mameynardi@gmail.com>
2025-06-20 18:56:37 -0300

    creacion del modelo de productos

---
+++ backend/models/productos.js
@@ -0,0 +1,47 @@
+const {DataTypes} = require('sequelize')
+const sequelize = require('../config/database.js');
+
+const PRODUCTOS = sequelize.define('PRODUCTOS',{
+    ID_PRODUCTO:{
+        allowNull:false,
+        type: DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    ID_TIPO:{
+        type: DataTypes.INTEGER,
+        allowNull:false,
+        references:{
+            model:'TIPOS_PRODUCTO',
+            key:'ID_TIPOS_PRODUCTO'
+        }
+    },
+    NOMBRE:{
+        type:DataTypes.STRING(100),
+        allowNull:false
+    },
+    DESCRIPCION:{
+        type:DataTypes.STRING(250),
+        allowNull:false
+    },
+    PRECIO:{
+        type: DataTypes.DOUBLE,
+        allowNull:false,
+    }, 
+    CANDTIDAD:{
+        type: DataTypes.INTEGER,
+        allowNull:false
+    },
+    URL_IMAGEN:{
+        type:DataTypes.STRING(250),
+        allowNull:false
+    },
+    PLATAFORMA:{
+        type:DataTypes.STRING(250),
+        allowNull:false
+    }
+},{
+    tableName:'PRODUCTOS'
+});
+
+module.exports = PRODUCTOS;
