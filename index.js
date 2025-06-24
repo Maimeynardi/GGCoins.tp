5aa6d07dd225a7538445430d7466e1491c9e4cf5
emmanuelalbe <emmanuelmalve@gmail.com>
2025-06-24 20:12:43 -0300

    index actualizado

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const databaseConection = require('./backend/configuracionDataBase/conexionBD.js')
+
 const app = express();
 
-const cors = require('cors')
+const cors = require('cors');
 
-app.use(cors())
+const port = 3030;
 
-app.use(express.json())
+app.use(cors());
+
+app.use(express.json());
+
+const PRODUCTOS = require('./backend/models/productos.js')
+const TIPOS_PRODUCTO = require('./backend/models/tipos_producto.js')
+const productosRouter = require('./backend/routes/productosRouter.js')
+
+
+app.use('/productos',productosRouter)
+app.listen(port,()=>{
+    databaseConection();
+    console.log(`Servidor Levantado`)
+})
