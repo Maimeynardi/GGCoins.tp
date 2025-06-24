069ad6c7cff3df2499eeae429fe9e15e92a1cd35
emmanuelalbe <emmanuelmalve@gmail.com>
2025-06-24 20:42:32 -0300

    Creando las Rutas de Prodctos, y Algunos Controladores de Productos

--- backend/controllers/productosControllers.js
+++ backend/controllers/productosControllers.js
@@ -1,4 +1,6 @@
+const { json } = require('sequelize');
 const PRODUCTOS = require('../models/productos.js');
+const { FOREIGNKEYS } = require('sequelize/lib/query-types');
 
 const obtenerProductos = async (req,res) =>{
     try {
@@ -10,7 +12,52 @@ const obtenerProductos = async (req,res) =>{
     }
 }
 
-module.exports = {
-    obtenerProductos
+const obtenerProductoPorID = async (req,res) =>{
+    try {
+        const producto = await PRODUCTOS.findByPk(req.params.id)
+        res.json(producto)
+    } catch (error) {
+        res.json({message:error.message})
+    }
+}
 
+
+const crearProducto = async (req,res) => {
+    try {
+        const productoNuevo = await PRODUCTOS.create(req.body);
+        res.json(productoNuevo);
+    } catch (error) {
+        res.json({message:error.message})
+    }
+}
+
+const eliminarProducto = async (req,res)=>{
+    try {
+        const productoEliminado = await PRODUCTOS.destroy({
+            WHERE:{id:req.params.id}
+        })
+        res.json(productoEliminado);
+    } catch (error) {
+        res.json({message:error.message})
+
+    }
+}
+
+const modificarProducto = async (req,res)=>{
+    try {
+        const modificarProducto = await PRODUCTOS.update(req.body,{
+            where:{id:req.params.id}
+        })
+        res.json(modificarProducto);
+    } catch (error) {
+        res.json({message:error.message})
+    }
+}
+
+module.exports = {
+    obtenerProductos,
+    obtenerProductoPorID,
+    crearProducto,
+    eliminarProducto,
+    modificarProducto
 }
