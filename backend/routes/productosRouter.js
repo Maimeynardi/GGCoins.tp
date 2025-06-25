8d5236de1adfc6c9ef111fed1db5820c9755a005
emmanuelalbe <emmanuelmalve@gmail.com>
2025-06-25 14:10:28 -0300

    modificandoRuta de CrearProducto

--- backend/routes/productosRouter.js
+++ backend/routes/productosRouter.js
@@ -6,7 +6,7 @@ const router  = express.Router();
 const {obtenerProductos,obtenerProductoPorID,crearProducto, eliminarProducto, modificarProducto} = require('../controllers/productosControllers')
 
 router.get('/',obtenerProductos);
-router.post('/',crearProducto);
+router.post('/crearProducto',crearProducto);
 router.delete('/:id',eliminarProducto)
 router.post('/:id',modificarProducto)
 router.get('/:id',obtenerProductoPorID)
