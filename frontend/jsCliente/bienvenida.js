afca34780c2438b1d4ed7c5a49f766e7a7ecb333
emmanuelalbe <emmanuelmalve@gmail.com>
2025-07-05 16:55:28 -0300

    Terminando

--- frontend/jsCliente/bienvenida.js
+++ frontend/jsCliente/bienvenida.js
@@ -1,19 +1,43 @@
-document.addEventListener("DOMContentLoaded", () => {
 
+document.addEventListener("DOMContentLoaded", () => {
+    
+    document.getElementById('botonBienvenida').addEventListener('click',guardarNombre)
+ 
 });
 
-const guardarNombreUsuario = () =>{
+const validarIngreso = () =>{
+    let bandera = true;
 
-    nombre =  document.getElementById('nombreUsuario').value;
+    let nombre =  document.getElementById('nombreUsuario');
+    nombre.classList.remove('is-invalid')
     let errorNombre = document.getElementById('validacionNombre');
 
     if(!nombre.value.trim() || nombre.value.length <3){
         nombre.classList.add('is-invalid')
         bandera = false;
         errorNombre.classList.add('invalid-feedback');
-        errorNombre.innerHTML = `Error, Debe ingresar el nombre del Producto`;
+        errorNombre.innerHTML = `Error, su nombre debe tener 3 letras`;
     }else{
         nombre.classList.add('is-valid');
         errorNombre.innerHTML = `Dato Correcto;`
+
     }
+
+    return bandera;
+
+}
+
+const guardarNombre = () =>{
+
+    if (validarIngreso()){
+        
+        let nombre = document.getElementById('nombreUsuario').value
+        
+        localStorage.setItem('NombreUsuario',nombre)
+        window.location.href = "/GGCoins.tp/frontend/cliente-html/productos.html";
+
+    }else{
+        conmsole.log('ingrese correctamente el nombre')
+    }
+    
 }
