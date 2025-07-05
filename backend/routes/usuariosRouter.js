024b3b7990f007c68902612d8ee24d6351880188
emmanuelalbe <emmanuelmalve@gmail.com>
2025-07-05 15:44:02 -0300

    solucionado como siempre jajajaja

--- backend/routes/usuariosRouter.js
+++ backend/routes/usuariosRouter.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router  = express.Router();
 
 const {obtenerUsuarios,obtenerUsuarioPorID,crearUsuario, eliminarUsuario, modificarUsuario, buscarAdmin} = require('../controllers/usuariosController')
-const { verificarToken, Admin } = require('../middleware/authMiddleware');
+const { verificarToken,} = require('../middleware/authMiddleware');
 const { login } = require('../controllers/auth.controller');
 //esta es de prueba 
 router.get('/',obtenerUsuarios);
 // esta es la que estaba primero router.get('/', verificarToken, obtenerUsuarios);
 router.post('/crearUsuario',crearUsuario);
-router.delete('/:id', verificarToken, Admin, eliminarUsuario);
+router.delete('/:id', verificarToken, eliminarUsuario);
 router.put('/:id', verificarToken, modificarUsuario);
 router.get('/:id', verificarToken, obtenerUsuarioPorID);
 module.exports = router;
