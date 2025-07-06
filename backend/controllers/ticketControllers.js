1b8b64837b7533eb2b5081353308a83b41625746
emmanuelalbe <emmanuelmalve@gmail.com>
2025-07-06 14:44:48 -0300

    agregando controlers ticket

---
+++ backend/controllers/ticketControllers.js
@@ -0,0 +1,9 @@
+const {TICKET} = require('../models/ticket');
+
+const cargarTicketPorId = async (req,res) =>{
+    try {
+        const ticket = await TICKET.fin
+    } catch (error) {
+        
+    }
+}
