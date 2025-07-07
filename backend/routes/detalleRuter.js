591b580c12ca9025349fa3699070f15256577e08
Maia Meynardi <156713722+Maimeynardi@users.noreply.github.com>
2025-07-06 22:50:02 -0300

    Delete backend/routes/detalleRuter.js

--- backend/routes/detalleRuter.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { crearVentaConDetalles } = require('../controllers/detalleVentaControllers.js');
-
-router.post('/crear', crearVentaConDetalles);
-module.exports = router;
